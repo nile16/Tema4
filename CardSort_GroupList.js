edcc380887ee825cef0abc944f8ea43d69b7b419
nile16 <nils.leandersson@gmail.com>
2016-10-09 21:45:48 +0200

    Add files via upload

---
+++ CardSort_GroupList.js
@@ -0,0 +1,20 @@
+var groupList = {
+	'Namnlös 1': {
+		'imgURL': ''
+	},
+	'Namnlös 2': {
+		'imgURL': ''
+	},
+	'Namnlös 3': {
+		'imgURL': ''
+	},
+	'Namnlös 4': {
+		'imgURL': ''
+	},
+	'Namnlös 5': {
+		'imgURL': ''
+	},
+	'Namnlös 6': {
+		'imgURL': ''
+	}
+}
